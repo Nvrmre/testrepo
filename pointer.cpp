a58cded4c2cedef1bec1dac3ad59e8e4f4a58cd8
Nvrmre <137487795+Nvrmre@users.noreply.github.com>
2023-07-01 14:15:23 +0700

    contoh pointer
    - langkah awal

---
+++ pointer.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	
+	// pointer adalah variable yang menyimpan alamat memori dari objek atau variabel lainnya &variabel atau &objek 
+	// adalah salah satu cara memakai pointer untuk mengetahui address
+	
+	int poin = 19;
+	int coba = 10;
+	
+	
+	
+	cout<<&poin<<endl;
+	
+	cout<<&coba<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
